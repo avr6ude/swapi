aad858095f64a6a5648370a6f30d6b951d6157b3
avr6ude <canuckchan@gmail.com>
2024-01-25 21:49:15 +0100

    removed jotai

--- src/models/store.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "jotai"
-
-export type Character = {
-  name: string
-}
-
-export const searchResultsAtom = atom<Character[]>([])
-export const searchTermAtom = atom<string>('')
